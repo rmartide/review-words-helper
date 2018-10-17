e1c1a00981dcda524e21d01a86d07d1f71f75d42
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-10-17 23:46:39 +0200

    Added empty card component for showing a word and behind the card the translation, or viceversa

---
+++ src/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+    <vue-fragment>
+        <div>TOP</div>
+    </vue-fragment>
+</template>
+
+<script>
+
+export default {
+    name: 'Card'
+}
+</script>
+
+<style>
+</style>
--- src/services/navigation.js
+++ src/services/navigation.js
@@ -1,8 +1,9 @@
 import Misc from '@components/Misc.vue';
 import Review from '@components/Review.vue';
-
+import Card from '@components/Card.vue';
 
 export const routes = [
     { path: '/misc', component: Misc, name: 'Misc' },
-    { path: '/review', component: Review, name: 'Review' }
+    { path: '/review', component: Review, name: 'Review' },
+    { path: '/cards', component: Card, name: 'Card' },
 ]
