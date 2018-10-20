b1a796a44389035397479c3b99177691d2cc27cd
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2018-10-20 23:59:43 +0200

    Added clases for rotating

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,11 +1,12 @@
 <template>
-    <div>
+    <div class="scene">
         <div class="rwh-card">
-            <div class="rwh-card__text">
+            <div class="rwh-card__face card__face--front">
                 FRONT
             </div>
+            <div class="rwh-card__face card__face--back">
+                BACK</div>
         </div>
-        <div>BACK</div>
     </div>
 </template>
 
@@ -24,19 +25,40 @@ export default {
 
 <style lang="scss">
 .rwh-card {
-  height: 300px;
-  width: 200px;
+  height: 100%;
+  width: 100%;
   border: 1px solid black;
   border-radius: 10px;
-  margin:auto;
-  &--front {
+  position: relative;
+  transition: transform 1s;
+  transform-style: preserve-3d;
+
+  &__face {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    backface-visibility: hidden;
+
+    &--front {
+      background: red;
+    }
+
+    &--back {
+      background: blue;
+      transform: rotateY(180deg);
+    }
   }
-  &--back {
-  }
-  &__text {
-    margin:auto;
-    display:table;
-  }
+
+  /* &__text {
+    margin: auto;
+    display: table;
+  } */
+}
+
+.scene {
+  height: 300px;
+  width: 200px;
+  perspective: 600px;
 }
 </style>
 
